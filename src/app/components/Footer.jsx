1b013094836aead1d5bfb7a6a8f366cd16e7a2f8
anointed osara <anointedosara@gmail.com>
2023-08-05 09:22:22 +0530

    new commit

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -115,10 +115,10 @@ const Wrapper = styled.div`
       flex-wrap: wrap !important;
       width: 100%;
       h1 {
-        font-size: 18px;
+        font-size: 9px;
       }
       p {
-        font-size: 9px;
+        font-size: 5px;
       }
     }
   }
